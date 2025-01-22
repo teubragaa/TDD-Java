3189fae783335cab10c0eb51cc52a8cd73787744
teubragaa <teubraga19@gmail.com>
2025-01-22 17:39:34 -0300

    Atividades de Desenvolvimento de Software Guiado por Testes - TDD

---
+++ src/pilha/PilhaCheiaException.java
@@ -0,0 +1,10 @@
+package pilha;
+
+public class PilhaCheiaException extends RuntimeException {
+
+
+	public PilhaCheiaException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/pilha/PilhaVaziaExcepction.java
@@ -0,0 +1,8 @@
+package pilha;
+
+public class PilhaVaziaExcepction extends RuntimeException {
+
+	public PilhaVaziaExcepction(String message) {
+		super(message);
+	}
+}
